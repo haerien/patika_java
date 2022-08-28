782b2fb0765ca5790cc49aab64c204a547f11a56
Emre Şahin <72073924+haerien@users.noreply.github.com>
2022-08-28 21:56:16 +0300

    Create daire_alani.java

---
+++ Java 101/daire_alani.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class daire_alani {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        double pi=3.14, area;
+      
+        System.out.print("Yari cap giriniz ="); int r = sc.nextInt();
+        System.out.print("Merkez acisinin olcusunu giriniz = "); int a = sc.nextInt();
+
+        area = (pi * (r*r) * a) / 360;
+
+        System.out.println("Dairenin alani  = " + area);
+        sc.close()
+    }
+}
